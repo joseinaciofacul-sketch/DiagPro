8006a27a0ab0ca3a9e40aa337b94ff631789d32d
Estagiário TI <estagiario.ti@PREFEITURADECASSIAMG.INTRANET>
2026-08-19 07:39:59 -0300

    Atualizar main.js e main.jsx

--- desktop/main.js
+++ desktop/main.js
@@ -1,5 +1,6 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, ipcMain } = require('electron')
 const path = require('path')
+const { detectarDispositivo } = require('./deviceDetector')
 
 function createWindow() {
   const win = new BrowserWindow({
@@ -7,10 +8,23 @@ function createWindow() {
     height: 800,
     autoHideMenuBar: true,
     icon: path.join(__dirname, 'public/logo.png'),
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      contextIsolation: true,
+      nodeIntegration: false,
+    },
   })
 
   win.loadURL('http://localhost:5173')
 }
+
+ipcMain.handle('detectar-dispositivo', async () => {
+  try {
+    return await detectarDispositivo()
+  } catch (err) {
+    return { conectado: false, erro: String(err) }
+  }
+})
 
 app.whenReady().then(createWindow)
 
--- desktop/src/main.jsx
+++ desktop/src/main.jsx
@@ -1,35 +1,9 @@
-const { app, BrowserWindow, ipcMain } = require('electron')
-const path = require('path')
-const { detectarDispositivo } = require('./deviceDetector')
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
 
-function createWindow() {
-  const win = new BrowserWindow({
-    width: 1280,
-    height: 800,
-    autoHideMenuBar: true,
-    icon: path.join(__dirname, 'public/logo.png'),
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
-      contextIsolation: true,
-      nodeIntegration: false,
-    },
-  })
-
-  win.loadURL('http://localhost:5173')
-}
-
-ipcMain.handle('detectar-dispositivo', async () => {
-  try {
-    return await detectarDispositivo()
-  } catch (err) {
-    return { conectado: false, erro: String(err) }
-  }
-})
-
-app.whenReady().then(createWindow)
-
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
